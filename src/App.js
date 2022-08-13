010a55cc9dd95a0ad44cea22823c64486384a43d
Igor <scandavee@gmail.com>
2022-08-13 18:50:02 +0300

    Start after tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.sass";
-import Calculator from "./components/Calculator/Calculator";
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <span>START</span>
     </div>
   );
 }
